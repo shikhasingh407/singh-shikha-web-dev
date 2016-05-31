2b2afd0a6c44cabbbc6bc6c864b4f666a6304c01
shikhasingh17 <singh.shik@husky.neu.edu>
2016-05-30 23:17:36 -0400

    Assignment 3: Final submission

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -1,24 +1,27 @@
-(function () {
+(function(){
     angular
         .module("WebAppMaker")
         .controller("ProfileController", ProfileController);
 
     function ProfileController($routeParams, UserService) {
         var vm = this;
-        var id = $routeParams.id;
+        vm.userId = $routeParams.id;
 
         vm.updateUser = updateUser;
 
-        function init() {
-            vm.user = UserService.findUserById(id);
+        function init(){
+            vm.user = UserService.findUserById(vm.userId);
         }
-
         init();
 
-        function updateUser(newUser){
-            UserService.updateUser(id, newUser);
+        function updateUser(newUser) {
+            if(UserService.updateUser(vm.userId, newUser)){
+                vm.success = "Your profile is saved!"
+            }
+            else{
+                vm.error = "OOPS!!! Unable to save the changes";
+            }
         }
     }
-
 
 })();
